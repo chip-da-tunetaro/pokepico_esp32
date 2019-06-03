c5fd870e74310c9c2ecdd3b9d49261f33fd3ad82
Akira <akira.matsuda@me.com>
2019-06-03 19:15:33 +0900

    Add MIDI over BLE interface.

--- components/pokepico/include/pokepico/pokepico.h
+++ components/pokepico/include/pokepico/pokepico.h
@@ -1,89 +1,39 @@
 #pragma once
 
+#include <MIDIBLE/MIDIBLE.h>
+#include <StreamLogger/Logger.h>
 #include <string>
-#include <BLEAdvertising.h>
-#include <BLEDevice.h>
 
 namespace pokepico
 {
-    const static std::string MidiServiceUUID = "03b80e5a-ede8-4b33-a751-6ce34ec4c700";
-    const static std::string MidiCharacteristicUUID = "7772e5db-3868-4112-a1a9-f2669d106bf3";
+using namespace m2d;
+using namespace m2d::ESP32;
+
 class Device
 {
 private:
-    std::string name;
-    uint8_t device_udid;
-    BLEServer *server;
-    BLEService *midi_service;
-    BLECharacteristic *midi_characteristic;
+	std::string name;
+	uint8_t device_udid;
+	MIDIBLE::BLEInterface *interface;
 
 public:
+	Device(std::string name, uint16_t udid)
+	{
+		this->interface = new MIDIBLE::BLEInterface(name, udid);
 
-    class ServerCallbacks : public BLEServerCallbacks
-    {
-    public:
-        std::function<void()> connect_handler = []() {};
-        std::function<void()> disconnect_handler = []() {};
-
-        void onConnect(BLEServer* server) {
-			connect_handler();
+		static Logger::Group logger("pokepico");
+		this->interface->note_on_handler = [&](MIDIBLE::MIDI::Channel channel, uint8_t note, uint8_t velocity) {
+			logger.info << "on >> \tnote : " << note << ", velocity: " << velocity << Logger::endl;
 		};
 
-        void onDisconnect(BLEServer* server) {
-			disconnect_handler();
-		}
-    };
-
-    class CharacteristicCallbacks : public BLECharacteristicCallbacks
-    {
-    public:
-        std::function<void(std::string)> read_handler = [](std::string) {};
-		std::function<void(std::string)> write_handler = [](std::string) {};
-
-		void onRead(BLECharacteristic *characteristic)
-        {
-			read_handler(characteristic->getValue());
-		}
-
-		void onWrite(BLECharacteristic *characteristic)
-        {
-			write_handler(characteristic->getValue());
-		}
-	};
-
-	ServerCallbacks service_callbacks;
-	CharacteristicCallbacks characteristic_callbacks;
-
-	Device(std::string name, uint16_t udid)
-    {
-        this->name = name;
-		this->device_udid = udid;
-
-		BLEDevice::init(this->name);
-		this->server = BLEDevice::createServer();
-		this->server->setCallbacks(&service_callbacks);
-		this->midi_service = server->createService(MidiServiceUUID.c_str());
-		this->midi_characteristic = midi_service->createCharacteristic(MidiCharacteristicUUID.c_str(),
-        BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_WRITE | 
-        BLECharacteristic::PROPERTY_NOTIFY | 
-        BLECharacteristic::PROPERTY_WRITE_NR);
-        this->midi_characteristic->setCallbacks(&characteristic_callbacks);
+		this->interface->note_off_handler = [&](MIDIBLE::MIDI::Channel channel, uint8_t note, uint8_t velocity) {
+			logger.info << "off >> \tnote : " << note << ", velocity: " << velocity << Logger::endl;
+		};
 	}
 
-    void begin()
-    {
-        this->midi_service->start();
-        
-        BLEAdvertising *advertising = this->server->getAdvertising();
-        BLEAdvertisementData advertising_data;
-
-        auto udid = std::string((const char*)&this->device_udid);
-        advertising_data.setFlags(0x04);
-        advertising_data.setManufacturerData(udid);
-        advertising_data.setCompleteServices(BLEUUID(MidiServiceUUID));
-        advertising->setAdvertisementData(advertising_data);
-        this->server->startAdvertising();
-    }
+	void begin()
+	{
+		this->interface->begin();
+	}
 };
 }
--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,6 @@
-#include <StreamLogger/Logger.h>
+#include <pokepico/pokepico.h>
 #include <sdkconfig.h>
 #include <string>
-#include <pokepico/pokepico.h>
 
 extern "C" {
 void app_main();
@@ -13,33 +12,5 @@ pokepico::Device *device;
 void app_main()
 {
 	device = new pokepico::Device("pokepico", 0x0c);
-
-	static Logger::Group logger("pokepico");
 	device->begin();
-	device->service_callbacks.connect_handler = [&] {
-		logger.info << "connected";
-		logger.info << Logger::endl;
-	};
-
-	device->service_callbacks.disconnect_handler = [&] {
-		logger.info << "disconnected";
-		logger.info << Logger::endl;
-	};
-
-	device->characteristic_callbacks.write_handler = [&] (std::string rx) {
-		// https://qiita.com/KazuyukiEguchi/items/e166ede5c97438b90187
-		char midi[5];
-		int pos = 0;
-		if (rx.length() > 0) {
-        for (int i = 0; i < rx.length(); i++) {
-          midi[pos] = rx[i];
-          pos++;
-          if(pos == 5) {
-			logger.info << midi[2] << midi[3] << midi[4];
-			logger.info << Logger::endl;
-            pos = 0;
-          }          
-        }
-      }
-	};
 }
